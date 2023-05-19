53fbd2a139887220392cb1c713e26c0863cba02c
AndersonJimenez <anjimhol23@gmail.com>
2023-05-18 19:12:38 -0500

    mi primer proyecto

---
+++ js/codigo.js
@@ -0,0 +1,56 @@
+function aleatorio (min, max){
+    return Math.floor(Math.random()*(max - min + 1) + min)
+}
+
+    function eleccion (jugada){
+        let resultado = ""
+            if(jugada == 1){
+            resultado = "piedra🧱"
+        } else if(jugada == 2){
+            resultado = "papel📃"
+        } else if(jugada == 3){
+            resultado = "tijera✂"
+        } else {
+            resultado = "MAL ELEGIDO☹"
+        }
+
+        return resultado
+
+    }
+
+// 1 es Pieda, 2 es papel, 3 es tijera
+let jugador = 0
+let pc = 0
+let triunfos = 0
+let perdidas = 0
+let empates = 0
+
+    while (triunfos < 3 && perdidas < 3 && empates < 3) {
+        pc = aleatorio (1,3)
+        jugador = prompt("Elige: 1 para pieda, 2 para papel y 3 para tijera")
+        //alert("elegiste "+jugador)
+
+        alert ("PC elige: " + eleccion(pc))
+        alert ("Tu eliges: " + eleccion(jugador))
+        
+        //COMBATE
+        if (pc == jugador){
+            alert("😮¡¡¡EMPATE!!!😮")
+            empates = empates + 1
+        } else if(jugador == 1 && pc == 3){
+            alert("😎¡¡¡GANASTE!!!😎")
+            triunfos = triunfos + 1
+        } else if(jugador == 2 && pc == 1){
+            alert ("😎¡¡¡GANASTE!!!😎")
+            triunfos = triunfos + 1
+        } else if(jugador == 3 && pc == 2){
+            alert ("😎¡¡¡GANASTE!!!😎")
+            triunfos = triunfos + 1
+        } else {
+            alert ("☹¡¡¡PERDISTE!!!☹")
+            perdidas = perdidas + 1
+        }
+        
+    }
+
+alert ("Ganaste " + triunfos + " veces, Perdiste " + perdidas  + " veces y empataste " + empates + " veces.")
